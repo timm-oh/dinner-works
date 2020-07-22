1faae1450e80f47ed3eb31eb5fcbcfcbdd4f5dc9
Tim McCarthy <omegoshi@gmail.com>
2020-07-22 19:31:46 +0200

    add: user sign in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'registrations#new'
-  # devise_for :users
+
+  devise_for :users
+
   resources :registrations, only: [:new, :create, :show]
 
-  # mount FieldTest::Engine, at: "field_test"
+  authenticate :user, ->(user) { user.admin? } do
+    mount FieldTest::Engine, at: "field_test"
+  end
 end
